62563819c94efbce3aec9eee5f78ab82a599cb1e
Roger Almeida <roger.eduardo@gmail.com>
2013-09-18 15:04:06 -0300

    Versao inicial

---
+++ manifests/default.pp
@@ -0,0 +1,8 @@
+exec { "apt-get update":
+    command => "/usr/bin/apt-get update",
+    onlyif => "/bin/sh -c '[ ! -f /var/cache/apt/pkgcache.bin ] || /usr/bin/find /etc/apt/* -cnewer /var/cache/apt/pkgcache.bin | /bin/grep . > /dev/null'",
+}
+package { "ipython-notebook":
+    ensure => "installed",
+    require  => Exec['apt-get update'],
+}
